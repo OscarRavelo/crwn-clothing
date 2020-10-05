46608bd228e2c90f29f55cb38febd844184126eb
Oscar Ravelo <oscar91ravelo@gmail.com>
2020-10-05 10:50:43 +0200

    added redux to the proyect

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <Provider store={store} >
+
   <React.StrictMode>
   <BrowserRouter>
 
     <App />
   </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
